e05a47d9c333d7fd866d9ffa59bd1d281bd13368
Giuseppe Mazzullo <peppenps@gmail.com>
2024-09-09 12:48:59 +0200

    Adds alert for video name not found

--- run.py
+++ run.py
@@ -112,6 +112,7 @@ async def main():
             video_name = None
             file_name = None
 
+
             if not video_name and len(replies_msg) > 0:
                 for replyMsg in replies_msg:
                     if replyMsg.reply_to_msg_id == message.id:
@@ -120,7 +121,6 @@ async def main():
             # Cerca il nome del file dal messaggio corrente
             if video_name is None:
                 video_name = sanitize_filename(message.text.split('\n')[0].strip()) if message.text else None
-
 
             # Codice esistente per trovare il file_name
             for attr in message.media.document.attributes:
@@ -133,6 +133,11 @@ async def main():
                 video_name = sanitize_filename(file_name.rsplit('.', 1)[0].strip())
 
             if video_name is None:
+                await client.send_message(
+                    message.peer_id,
+                    messages['empty_reference_specify_name'],
+                    reply_to=message.id
+                )
                 continue
 
             if file_name is None:
