9e0d12bbcf434751b7ae63047ae187aad45d6fe1
AnimaL Sina <peppenps@gmail.com>
2024-11-04 21:23:15 +0100

    Fixes on chat.chat missing data

--- func/rules.py
+++ func/rules.py
@@ -83,8 +83,11 @@ def apply_rules(type_name, input_value, chat = None):
             rule_chat_title = pattern.chat_title or None
             rule_chat_name = pattern.chat_name or None
             chat_id = chat.chat_id or None
-            chat_title = chat.chat.title or None
-            chat_name = chat.chat.username or None
+            chat_title = None
+            chat_name = None
+            if chat.chat is not None:
+                chat_title = chat.chat.title
+                chat_name = chat.chat.username
             if rule_chat_id is not None and rule_chat_id != chat_id:
                 continue
             if rule_chat_name is not None and rule_chat_name != chat_name:
