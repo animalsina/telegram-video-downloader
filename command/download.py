b2a65b70430f68ddf5568df55b175b9560636945
AnimaL Sina <peppenps@gmail.com>
2024-11-12 00:24:34 +0100

    Adds pin and unpin for priority and add dynamic wait time for flood premium wait

--- command/download.py
+++ command/download.py
@@ -3,9 +3,11 @@ Command download
 """
 
 from classes.object_data import ObjectData
-from func.main import configuration, operation_status
+from classes.string_builder import LINE_FOR_PINNED_VIDEO
+from func.main import configuration
 from func.messages import t
 from func.telegram_client import edit_service_message
+from func.utils import save_video_data, add_line_to_text
 
 
 async def run(  # pylint: disable=unused-argument
@@ -31,6 +33,8 @@ async def run(  # pylint: disable=unused-argument
         await rename(extra_args.get('target'), text_input, callback)
     elif subcommand in ('target', 'dir', 'destination'):
         await target_to_download(extra_args.get('target'), extra_args.get('reply_message'))
+    elif subcommand == 'pin' or subcommand == 'unpin':
+        await set_pinned_message(extra_args.get('target'), extra_args.get('reply_message'), subcommand == 'pin')
 
 
 async def start(message, callback):
@@ -74,3 +78,22 @@ async def target_to_download(target, video_object: ObjectData):
         'completed_folder_mask',
         video_object.video_name, message_id=video_object.video_id)
     await edit_service_message(target, completed_folder_mask or configuration.completed_folder)
+
+
+async def set_pinned_message(target, video_object: ObjectData, pinned: bool):
+    """
+    :param target:
+    :param video_object:
+    :param pinned:
+    :return:
+    """
+
+    if pinned:
+        await edit_service_message(target, t('pinned_message', video_object.video_name))
+    else:
+        await edit_service_message(target, t('unpinned_message', video_object.video_name))
+    save_video_data({"pinned": pinned}, video_object, ["pinned"])
+    await add_line_to_text(
+        video_object.message_id_reference,
+        str(pinned),
+        LINE_FOR_PINNED_VIDEO, True)
