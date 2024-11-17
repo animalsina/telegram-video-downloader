5846e066aac425b8c394d1c9ef023f9ac4c41b8b
AnimaL Sina <peppenps@gmail.com>
2024-11-17 12:45:55 +0100

    fix: adds exceptions and new logic for rules delete action

--- func/main.py
+++ func/main.py
@@ -271,19 +271,17 @@ async def main():  # pylint: disable=unused-argument, too-many-statements
                     rule_data = operation_status.rules_registered[del_message_id]
                     shutil.move(rule_data.file_name,
                                 rule_data.file_name + '.deleted')  # Move the file to a deleted folder
-                    rules_object.reload_rules()
-                    operation_status.can_delete_rules = False
-                    await client.delete_messages(
-                        PERSONAL_CHAT_ID,
-                        message_ids=list(operation_status.rules_registered.keys()))
-                    await send_service_message(
-                        PERSONAL_CHAT_ID, t('rules_reloaded'))
                     await send_service_message(
                         PERSONAL_CHAT_ID, t('rule_deleted', rule_data.file_name))
 
             for message_id in event.deleted_ids:
-                remove_video_data(get_inlist_video_object_by_message_id_reference(message_id))
+                video_object = get_inlist_video_object_by_message_id_reference(message_id)
+                if video_object is not None:
+                    remove_video_data(video_object)
                 await remove_rules(message_id)
+            if operation_status.can_delete_rules is True:
+                rules_object.reload_rules()
+                await send_service_message(PERSONAL_CHAT_ID, t('rules_reloaded'))
 
         async def tg_new_message_handler(event: NewMessage.Event):
             """NewMessage Handler"""
