4ff17d1573b1e0a21a9a39152a068f2d556d2f63
AnimaL Sina <peppenps@gmail.com>
2025-02-09 23:08:50 +0100

    feat: adds a sample of plugin

--- func/plugin_manager.py
+++ func/plugin_manager.py
@@ -1,14 +1,39 @@
+"""
+Plugin Manager class.
+
+This class is used to load and manage plugins. A plugin is a python file
+in the plugin directory. Each plugin is loaded and the function with the
+name that is the same as the plugin name is called. The function should
+return a dictionary with the following structure:
+{
+    "filter_name": function,
+    "filter_name_2": function_2,
+    ...
+}
+"""
 import os
 import importlib.util
 
 
 class PluginManager:
+    """
+    Plugin Manager
+    """
     def __init__(self, plugin_dir='plugins'):
         self.plugin_dir = plugin_dir
         self.filters = {}
 
     def load_plugin(self, plugin_name):
-        """Carica dinamicamente un plugin da un file Python."""
+        """
+        Load a plugin by its name.
+
+        Args:
+            plugin_name (str): The name of the plugin to load.
+
+        Returns:
+            module: The loaded plugin module or None if the plugin does not exist.
+
+        """
         plugin_path = os.path.join(self.plugin_dir, f'{plugin_name}.py')
         if os.path.exists(plugin_path):
             spec = importlib.util.spec_from_file_location(plugin_name, plugin_path)
@@ -18,7 +43,21 @@ class PluginManager:
         return None
 
     def apply_filters(self, function_name, value):
-        """Applica tutte le funzioni dei plugin a un valore."""
+        """
+        Apply a sequence of filter functions from plugins to a given value.
+
+        This method iterates through all the Python files in the plugin directory,
+        loads each plugin, and checks if the specified function exists within the plugin.
+        If the function is found, it is applied to the input value.
+
+        Args:
+            function_name (str): The name of the function to apply from each plugin.
+            value: The initial value to be filtered by the plugin functions.
+
+        Returns:
+            The value after being processed by all applicable plugin functions.
+
+        """
         for plugin_file in os.listdir(self.plugin_dir):
             if plugin_file.endswith('.py'):
                 plugin_name = plugin_file[:-3]  # Rimuovi l'estensione .py
--- plugins/parse_movie_name.py
+++ plugins/parse_movie_name.py
@@ -1,27 +1,72 @@
-import requests
-from bs4 import BeautifulSoup
+"""
+EXAMPLE OF A PLUGIN
 
-def parse_file_name(video_name):
-    new_name = get_movie_name_from_tvdb(video_name)
+Plugin to parse the video name and try to find the correct title of a movie/TV series.
+Uses the Gemini AI model to analyze the name and find the correct title.
+If the title is not found, returns the original video name.
+"""
 
-    return f"modificato_{new_name}"
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
 
-def get_movie_name_from_tvdb(video_name):
-    search_url = f"https://www.themoviedb.org/search?query={video_name.replace(' ', '+')}"
+load_dotenv()
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+rules = os.getenv("PROMPT_RULES", "")
+active_plugin = os.getenv("ACTIVE_PLUGIN", None)
 
-    response = requests.get(search_url, headers=headers)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
+GENAI_API_KEY = os.getenv('GEMINI_API_KEY', None)
 
-        # Trova il primo risultato nella sezione film
-        movie_element = soup.select_one(".results .card .title a")
+genai.configure(api_key=GENAI_API_KEY)
 
-        if movie_element:
-            return movie_element.text.strip()
+rule_list = rules.splitlines() if rules else []
 
-    return None
+
+def parse_file_name(video_name):  # pylint: disable=unused-argument
+    """
+    Plugin to parse the video name and try to find the correct title of a movie/TV series.
+    Uses the Gemini AI model to analyze the name and find the correct title.
+    If the title is not found, returns the original video name.
+    :param video_name:
+    """
+    if active_plugin is None:
+        return video_name
+
+    new_name = gemini_return_the_title(video_name)
+    if new_name is None:
+        return video_name
+    return new_name
+
+
+def gemini_return_the_title(query) -> str | None:
+    """
+    Uses the Gemini AI model to analyze the name and find the correct title.
+    If the title is not found, returns None.
+
+    Returns:
+        object:
+    """
+    prompt = (
+        f"What is the correct title of a movie or TV series called '{query}'?"
+        f" From the title I provided, extract the name, look for possible associations."
+        f" Perform proper analysis on the name, it likely exists even if spelled incorrectly."
+        f" Just make sure that by the end of your analysis,"
+        f" the resulting name is indeed a movie or TV series."
+        f" In conclusion, return only the correct name with no additional text."
+        f" Your output is needed to retrieve the title,"
+        f" so the rest of your thoughts are unnecessary. Thank you!"
+        f" Finally, if you believe the title I provided doesn't exist at all, "
+        f" return a text exactly like \"false\".")
+
+    for rule in rule_list:
+        prompt += f" {rule.strip()}"
+
+    try:
+        model = genai.GenerativeModel("gemini-pro")
+        response = model.generate_content(prompt)
+        if response.text == 'false':
+            return None
+        return response.text
+    except Exception:  # pylint: disable=broad-exception-caught
+        return None
